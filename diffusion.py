b952ac69541ce08ce86c496a32ee84d08b8e51c0
Blaz Blokar <blaz@katalist.ai>
2025-09-02 22:36:07 +0200

    add film

--- diffusion.py
+++ diffusion.py
@@ -30,24 +30,55 @@ def t_embed(t, dim=32):
 #    The y_emb is the embedding of the color name eg. label
 #    x_t is the RGB of the color at the current timestep
 #    t_cont is the timestep as a continuous value between 0 and 1
-#    The output is the predicted noise ε_hat
+#    The output is the predicted noise ε_hat, a 3 value vector in [-1,1]
 #  Architecture:
 #    - yproj: Project the label embedding to 64 dimensions
-#    - fc: 3-layer MLP to predict the noise with SiLU activation
+#    - hidden layer: 256 dimensions
+#    - FiLM: Factorized Layer Normalization
+#       - a way to inject the label embedding into the MLP continuously instead of just at the input
+#    - SiLU activation - a smooth activation function that is similar to ReLU but has a smooth curve
 # ---------------------------
 class EpsMLP(nn.Module):
-    def __init__(self, ydim, h):
+    def __init__(self, y_in=384, y_proj=64, h=256, tdim=32):
         super().__init__()
-        self.yproj = nn.Sequential(nn.Linear(384, 64), nn.LayerNorm(64), nn.SiLU())
-        self.fc = nn.Sequential(
-            nn.Linear(3 + 32 + 64, h), nn.SiLU(),
-            nn.Linear(h, h), nn.SiLU(),
-            nn.Linear(h, 3)
-        )
+
+        self.yproj = nn.Sequential(nn.Linear(y_in, y_proj), nn.LayerNorm(y_proj), nn.SiLU())
+        self.in_lin = nn.Linear(tdim + 3, h)
+
+        # h * 2 because we have scale and shift (eg. gamma * x + beta)
+        self.in_film = nn.Linear(y_proj, 2 * h)
+        # initialize to zero
+        nn.init.zeros_(self.in_film.weight)
+        nn.init.zeros_(self.in_film.bias)
+
+        self.h_lin = nn.Linear(h, h)
+        self.h_film = nn.Linear(y_proj, 2 * h)
+        nn.init.zeros_(self.h_film.weight)
+        nn.init.zeros_(self.h_film.bias)
+
+        self.out_lin = nn.Linear(h, 3)
+
     def forward(self, x_t, t_cont, y):
         te = t_embed(t_cont)
-        h = torch.cat([x_t, te, self.yproj(y)], dim=1)
-        return self.fc(h) # ε_hat
+        y = self.yproj(y)
+
+        h = torch.cat([x_t, te], dim=1)
+
+        # input layer
+        h = self.in_lin(h)
+        g_in, b_in = self.in_film(y).chunk(2, dim=1) # split into two tensors
+        h = h * (g_in + 1) + b_in # apply film scale and shift
+
+        h = F.silu(h) # activation
+
+        # hidden layer
+        h = self.h_lin(h)
+        g_h, b_h = self.h_film(y).chunk(2, dim=1)
+
+        h = h * (g_h + 1) + b_h
+        h = F.silu(h)
+
+        return self.out_lin(h) # ε_hat
 
 # ---------------------------
 # 4) Beta schedule + ᾱ precompute
@@ -108,7 +139,7 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     data = make_dataset()
     txtenc = SentenceTransformer("all-MiniLM-L6-v2").to(device)
-    model  = EpsMLP(384, 128).to(device)
+    model  = EpsMLP().to(device)
     opt    = torch.optim.AdamW(list(model.parameters()), lr=2e-3)
     _,_,abar = make_alpha_bar(T=1000, device=device)
 
